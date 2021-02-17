587ed22ba430a082202e12503102656424433a88
Astha <16asthanema@gmail.com>
2021-02-17 16:59:35 +0530

    initial commit

---
+++ src/app/calc/calc.component.ts
@@ -0,0 +1,19 @@
+import {Component} from '@angular/core';
+
+@Component({
+selector: 'calc',
+templateUrl: './calc.component.html',
+styleUrls:['./calc.component.css']
+
+})
+
+export class CalcComponent{
+public number1? : number;
+public number2? : number;
+public result?:number
+
+public add(){
+    if(this.number1&& this.number2)
+this.result= this.number1 + this.number2;
+}
+}
